4af753678482b1bfddd5f61ce5aff9ae2548d1e5
xiaohao666666666666666 <jiuzhyecizhuceyiqmeiy@outlook.com>
2024-12-06 19:14:27 +0800

    connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -41,7 +41,7 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
   double? get height => bind.isIncomingOnly() ? null : em * 3;
 
   void onUsePublicServerGuide() {
-    const url = "https://rustdesk.com/pricing.html";
+    const url = "https://t.me/liran1226";
     canLaunchUrlString(url).then((can) {
       if (can) {
         launchUrlString(url);
