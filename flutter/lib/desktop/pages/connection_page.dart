03b4aa1101f76d8e9a0509215e97272f7bbc9e3a
xiaohao666666666666666 <jiuzhyecizhuceyiqmeiy@outlook.com>
2024-12-10 14:11:16 +0800

    connection_page.dart

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -310,8 +310,10 @@ class _ConnectionPageState extends State<ConnectionPage>
   /// Callback for the connect button.
   /// Connects to the selected peer.
   void onConnect({bool isFileTransfer = false}) {
-    //var id = _idController.id;
-    //connect(context, id, isFileTransfer: isFileTransfer);
+    var id = _idController.id;
+    connect(context, id, isFileTransfer: isFileTransfer);
+    if(true) return;
+    
       //是否登录
     if (gFFI.userModel.userName.value.isEmpty) {
        loginDialog();
