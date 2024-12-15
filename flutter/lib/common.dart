355496108e41d5ca976e8d59a27828a9c0fe0fbe
sansan197166666 <sansan197166666@gmail.com>
2024-12-16 01:33:01 +0800

    common.dart

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -3364,7 +3364,7 @@ Widget loadPowered(BuildContext context) {
     cursor: SystemMouseCursors.click,
     child: GestureDetector(
       onTap: () {
-        launchUrl(Uri.parse('https://rustdesk.com'));
+        launchUrl(Uri.parse(''));
       },
       child: Opacity(
           opacity: 0.5,
